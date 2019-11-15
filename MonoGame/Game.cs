974f3a5d7135c60c529a745db710f2e3d763e4f1
Michal-MK <michalhz159@gmail.com>
2019-11-15 09:03:24 +0100

    Initial Commit - contains the structure of a MonoGame project - + some additional helper functions 	OpenSimplex Noise generator 	HSV -> RGB converter 	Primitive drawing

---
+++ MonoGame/Game.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame {
+	public class Game : Microsoft.Xna.Framework.Game {
+		private readonly GraphicsDeviceManager _gdManager;
+		private readonly SpriteBatch _renderer;
+
+		public Game() {
+			_gdManager = new GraphicsDeviceManager(this);
+			_renderer = new SpriteBatch(GraphicsDevice);
+			Content.RootDirectory = "Content";
+		}
+
+		protected override void Initialize() {
+			//Inital setup, runs once. (Základní nastaení, voláno jednou.)
+			IsMouseVisible = true;
+			_gdManager.ApplyChanges();
+			base.Initialize();
+		}
+
+		protected override void LoadContent() {
+			// Load textures/content (Načti textury/jiný obsah)
+			base.LoadContent();
+		}
+
+		protected override void Update(GameTime gameTime) {
+			// Update game logic/state (Akualizuj stav hry/herní logiky)
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime) {
+			GraphicsDevice.Clear(Color.Black);
+			_renderer.Begin(blendState: BlendState.AlphaBlend);
+			// Draw to the screen (Vykresli stav na obrazovku)
+			_renderer.End();
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ MonoGame/Helpers/ValueMapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MonoGame {
+	public static class ValueMapper {
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static double Map(double input, double inFrom, double inTo, double outFrom, double outTo) {
+			double slope = 1.0 * (outTo - outFrom) / (inTo - inFrom);
+			return outFrom + slope * (input - inFrom);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Map(int input, int inFrom, int inTo, int outFrom, int outTo) {
+			int slope = (outTo - outFrom) / (inTo - inFrom);
+			return outFrom + slope * (input - inFrom);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int Map(double input, double inFrom, double inTo, int outFrom, int outTo) {
+			double slope = (outTo - outFrom) / (inTo - inFrom);
+			return (int)(outFrom + slope * (input - inFrom));
+		}
+	}
+}
